b643a2cf6c93be320a6839f53d3c901860a4d28e
coderhero5 <abdulxakimtojiyev2004@gmail.com>
2022-12-14 21:44:28 +0500

    java6

---
+++ app.js
@@ -0,0 +1,51 @@
+// let a=5;
+// if (a>0) {
+//     console.log(++a);
+// }else{
+//     console.log(a);
+// }
+
+
+// let a = 5;
+// if (a > 0) {
+//   console.log(a= - 2);
+// } else {
+//   console.log(a);
+// }
+
+
+// const a = prompt("son kiriting");
+// let user = a;
+// if (user > 0) {
+//   console.log(++user);
+// } else if (user < 0) {
+//   console.log(user - 2);
+// } else if (user == 0) {
+//   console.log(+10);
+// }
+
+// let a = +prompt("son kiritib koring ");
+// let b = +prompt();
+// let c = +prompt();
+// let d=0;
+// if (a > 0) {
+//     d++;
+// }
+// if (b > 0) {
+//     d++;
+// }
+// if (c > 0) {
+//     d++;
+// }
+// console.log(d);
+
+let a = +prompt("son kiritib koring");
+let b = +prompt("son kiritib koring");
+let c = +prompt("son kiritib koring");
+
+if ( a > 0, b>0 , c > 0) {
+    console.log("3 ta musbat");
+} 
+else if ((a > 0, b > 0, c < 0) || (a > 0, b < 0, c > 0) || (a < 0 , b> 0 ,c > 0) ){
+    console.log("2 ta musbat va 1ta manfiy");
+}
